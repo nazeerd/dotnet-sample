01f91b3fc1e476e75c2ac19a43c4ff0cdda332eb
nazeerd <dudekulanazeerahammad@gmail.com>
2023-11-18 20:59:10 +0530

    Sample data

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linqsample1
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8 };
+            var mixedsyntax = (from obj in list
+                               select obj).Max();
+
+
+
+            Console.WriteLine("MaxValue value= " + mixedsyntax);
+
+                Console.ReadLine();
+
+            
+
+        }
+    }
+}
+
+
+
+
+
+
+
