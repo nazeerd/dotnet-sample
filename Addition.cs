e46f786bf0df39517dd680b2b320bd6082e58c2a
cpreethi66 <cpreethi66@gmail.com>
2023-11-20 22:02:37 +0530

    Sample-Programm

---
+++ Addition.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace introduction_to_csharp
+{
+    public class Addition
+    {
+        public int Add(int a, int b)
+        {
+            int c = a + b;
+            return (c);
+        }
+    }
+}
